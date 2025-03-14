5ac362e1615762ca9c978d462616bc9a3c5bb06d
LAPTOP-I3UDFKIG\user <qweasd34794788@gmail.com>
2025-03-14 15:12:53 +0800

    刪除功能

--- src/main/java/com/example/WuyeGuanli/service/ifs/FinancialService.java
+++ src/main/java/com/example/WuyeGuanli/service/ifs/FinancialService.java
@@ -1,6 +1,7 @@
 package com.example.WuyeGuanli.service.ifs;
 
 import com.example.WuyeGuanli.vo.FinancialAddInfoReq;
+import com.example.WuyeGuanli.vo.FinancialDelectReq;
 import com.example.WuyeGuanli.vo.FinancialSearchReq;
 import com.example.WuyeGuanli.vo.FinancialSearchRes;
 
@@ -9,4 +10,6 @@ public interface FinancialService {
 	public int addInfo(FinancialAddInfoReq req);
 	
 	public FinancialSearchRes search(FinancialSearchReq req);
+	
+	public int delect(FinancialDelectReq req);
 }
